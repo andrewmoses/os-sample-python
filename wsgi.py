11359f9c29c0afd39873a7cd1c04069c7f81c41b
Andrew Moses` <andrew2moses@gmail.com>
2018-07-24 05:34:32 -0400

    app to application

--- wsgi.py
+++ wsgi.py
@@ -4,11 +4,11 @@ import os
 
 application = Flask(__name__)
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_DATABASE_USER']
-app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_DATABASE_PASSWORD']
-app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB']
-app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_DATABASE_HOST']
-mysql.init_app(app)
+application.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_DATABASE_USER']
+application.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_DATABASE_PASSWORD']
+application.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB']
+application.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_DATABASE_HOST']
+mysql.init_app(application)
 
 @application.route("/")
 def hello():
